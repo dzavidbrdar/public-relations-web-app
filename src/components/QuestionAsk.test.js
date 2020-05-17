efbb9667327d8e76591465d281b55c40e9a0daf1
Emir Kurtovic <emir-4@hotmail.com>
2020-05-17 17:05:53 +0200

    dodatni unit testovi

---
+++ src/components/QuestionAsk.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import QuestionAsk from './QuestionAsk.js';
+import {render,fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+
+it('renders without crashing',()=>{
+  const div = document.createElement('div');
+  ReactDOM.render(<QuestionAsk />, div);
+});
+
+it('prikazuje poruku kad je pitanje krace od 8 karaktera',()=>{
+  const {getByPlaceholderText,getByTestId,getByText}=render(<QuestionAsk/>);
+  const inputPitanje=getByPlaceholderText('Question');
+  fireEvent.change(inputPitanje,{target:{value:'a?'}});
+  getByText('Question must be 8 characters long!');
+});
+/*dodati eventualno za mock sa fake userom*/
