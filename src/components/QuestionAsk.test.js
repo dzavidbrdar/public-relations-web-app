0b275a3d89ff740c29275f511e024fa2bf9154d5
Emir Kurtovic <emir-4@hotmail.com>
2020-05-17 17:36:37 +0200

    opet prolaze svi unit testovi

--- src/components/QuestionAsk.test.js
+++ src/components/QuestionAsk.test.js
@@ -14,6 +14,6 @@ it('prikazuje poruku kad je pitanje krace od 8 karaktera',()=>{
   const {getByPlaceholderText,getByTestId,getByText}=render(<QuestionAsk/>);
   const inputPitanje=getByPlaceholderText('Question');
   fireEvent.change(inputPitanje,{target:{value:'a?'}});
-  getByText('aa');
+  getByText('Question must be 8 characters long!');
 });
 /*dodati eventualno za mock sa fake userom*/
